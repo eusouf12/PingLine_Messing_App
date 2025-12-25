80645a24e096a1a54e87571a62128e1747b87fe2
eusouf12 <riyajuddeneusouf@gmail.com>
2025-12-26 04:02:21 +0600

    connect my home mac

--- lib/view/screens/authentication/views/auth_screen/login_screen/login_screen.dart
+++ lib/view/screens/authentication/views/auth_screen/login_screen/login_screen.dart
@@ -20,6 +20,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
+
     return Scaffold(
         backgroundColor: AppColors.primary,
         body: SingleChildScrollView(
